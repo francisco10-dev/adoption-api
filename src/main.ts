3d9dd542552b06bb90e5bbfffd09ef642d19aad9
Francisco Ortiz <jose.ortiz.ortiz@est.una.ac.cr>
2024-06-09 08:37:50 -0600

    Información de rutas del api

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,28 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { JwtMiddleware } from './middleware/jwt.middleware';
 
+const endPoints = {
+  GRAPHQL: 'http://localhost:9000/graphql',
+  LOGIN: 'http://localhost:9000/auth/login',
+  VALIDAR_EMAIL:'http://localhost:9000/auth/validate-email/:email',
+  OBTENER_IMAGEN: 'http://localhost:9000/images/:filename'
+}
+
+const routes = Object.entries(endPoints).map(([accion, ruta]) => ({
+  Acción: accion,
+  Ruta: ruta
+}));
+
+const showRoutes = () => {
+  console.log('RUTAS: ')
+  console.table(routes);
+}
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.use(new JwtMiddleware().use);
   await app.listen(9000);
-  console.log('http://localhost:9000/graphql')
+  showRoutes();
 }
 bootstrap();
